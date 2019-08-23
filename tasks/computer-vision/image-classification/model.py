31282e5343afc7d33a74595c8686dd171502b735
daniekbrink <daniek@praelexis.com>
2019-08-23 18:26:46 +0200

    update model defaults

--- tasks/computer-vision/image-classification/model.py
+++ tasks/computer-vision/image-classification/model.py
@@ -6,7 +6,7 @@ import os
 import mlflow
 import mlflow.keras
 from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.layers import Dense, Reshape
+from tensorflow.python.keras.layers import Dense, Reshape, Flatten, Concatenate
 from tensorflow.python.keras import optimizers
 from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
